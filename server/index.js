69eb9fa2e2f556b25b362782e3a2fefb9943e87a
Aditya <ranjan.aditya2901@gmail.com>
2023-09-22 19:43:09 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const port =  10050;
 
 app.use(require("./route/auth"));
 if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
     const path = require("path");
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'client/build/index.html'));
+        res.sendFile(path.join(__dirname + 'frontend/build/index.html'));
     })
 }   
 // app.use(express.json());
